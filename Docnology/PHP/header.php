7de614371debe5d24f00bd1474d88a298ff99fbb
DavidFromComSci <davidbyfield@hotmail.com>
2022-06-29 22:25:14 -0500

    converted to php

---
+++ Docnology/PHP/header.php
@@ -0,0 +1,30 @@
+<html>
+    <img src="../Images/img/shape.png" alt="" class="shape">
+
+            <header>
+                <div class="container">
+                    <div class="logo">
+                        <img src="../Images/img/logo.png" alt="Logo">
+                        <h3>Docnology</h3>
+                    </div>
+
+                    <div class="links">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="../PHP/patient-profile.php">Profile</a></li>
+                            <li><a href="#">Chat Room</a></li>
+                            <li><a href="../PHP/appointment.php">Appointments</a></li>
+                            <li><a href="#">My History</a></li>
+                            <li><a href="#">Make payment</a></li>
+                            <li><a href="Signup.html" class="btn">Log Out</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="overlay"></div>
+
+                    <div class="hamburger-menu">
+                        <div class="bar"></div>
+                    </div>
+                </div>
+            </header>
+</html>
